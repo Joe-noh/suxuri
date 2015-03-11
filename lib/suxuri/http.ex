dd72247fcac8303a7aa9daf97b41892caa1b75d3
Joe-noh <goflb.jh@gmail.com>
2015-03-11 23:08:29 +0900

    user api

--- lib/suxuri/http.ex
+++ lib/suxuri/http.ex
@@ -9,6 +9,7 @@ defmodule Suxuri.HTTP do
   @base_url "https://suzuri.jp/api/v1"
 
   # TODO: alignment :P
+  def get(path), do: H.get(full(path), headers)
   def get(path, params), do: H.get(full(path), headers, params: params)
   def post(path, params), do: H.post(full(path), JSX.encode!(params), headers)
   def put(path, params), do: H.put(full(path), JSX.encode!(params), headers)
--- lib/suxuri/user.ex
+++ lib/suxuri/user.ex
@@ -1,2 +1,23 @@
 defmodule Suxuri.User do
+  alias Suxuri.HTTP
+
+  def get(user_id) do
+    HTTP.get("/users/#{user_id}")
+  end
+
+  def self do
+    HTTP.get("/user")
+  end
+
+  def update(params) when is_map(params) do
+    HTTP.put("/user", params)
+  end
+
+  def update_name(name) when is_binary(name) do
+    HTTP.put("/user", %{"displayName" => name})
+  end
+
+  def update_avatar(url) when is_binary(url) do
+    HTTP.put("/user", %{"avatarUrl" => url})
+  end
 end
