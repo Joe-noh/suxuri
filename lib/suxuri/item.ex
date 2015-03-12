84e6198551836c311751c6432c61e8745789360c
Joe-noh <goflb.jh@gmail.com>
2015-03-12 11:05:09 +0900

    define item struct and its child

--- lib/suxuri/item.ex
+++ lib/suxuri/item.ex
@@ -1,4 +1,18 @@
 defmodule Suxuri.Item do
+  defmodule Variant do
+    defmodule Color do
+      defstruct [:id, :name, :rgb]
+    end
+
+    defmodule Size do
+      defstruct [:id, :name]
+    end
+
+    defstruct [:id, :price, :exemplary, :color, :size]
+  end
+
+  defstruct [:id, :name, :angles, :humanize_name, :variants]
+
   alias Suxuri.HTTP
 
   def list do
