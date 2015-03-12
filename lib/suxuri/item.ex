8057bebcd810ef1ec955bb5ef9cb24f0af144d4a
Joe-noh <goflb.jh@gmail.com>
2015-03-12 12:40:43 +0900

    Item.list/0 returns item structs

--- lib/suxuri/item.ex
+++ lib/suxuri/item.ex
@@ -1,21 +1,55 @@
 defmodule Suxuri.Item do
+  alias Suxuri.HTTP
+
   defmodule Variant do
     defmodule Color do
       defstruct [:id, :name, :rgb]
+
+      def new(%{"id" => id, "name" => name, "rgb" => rgb}) do
+        %__MODULE__{id: id, name: name, rgb: rgb}
+      end
     end
 
     defmodule Size do
       defstruct [:id, :name]
+
+      def new(%{"id" => id, "name" => name}) do
+        %__MODULE__{id: id, name: name}
+      end
     end
 
     defstruct [:id, :price, :exemplary, :color, :size]
+
+    def new(%{"id" => id, "price" => price, "exemplary" => exemplary,
+              "color" => color, "size" => size}) do
+      %__MODULE__{id: id, price: price, exemplary: exemplary,
+                  color: Color.new(color), size: Size.new(size)}
+    end
+
+    def from_list(variants) when is_list(variants) do
+      from_list(variants, [])
+    end
+
+    defp from_list([head | rest], acc), do: from_list(rest, [new(head) | acc])
+    defp from_list([], acc), do: Enum.reverse acc
   end
 
   defstruct [:id, :name, :angles, :humanize_name, :variants]
 
-  alias Suxuri.HTTP
+  def new(%{"id" => id, "name" => name, "angles" => angles,
+            "humanizeName" => humanize_name, "variants" => variants}) do
+    %__MODULE__{id: id, name: name, angles: angles, humanize_name: humanize_name,
+                variants: Variant.from_list(variants)}
+  end
+
+  def from_list(items) when is_list(items) do
+    from_list(items, [])
+  end
+
+  defp from_list([head | rest], acc), do: from_list(rest, [new(head) | acc])
+  defp from_list([], acc), do: Enum.reverse acc
 
   def list do
-    HTTP.get!("/items")
+    HTTP.get!("/items") |> Map.get("items") |> from_list
   end
 end
