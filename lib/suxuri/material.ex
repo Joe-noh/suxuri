62ee7941a7c1c8219bafaff92ccf656588a03c9b
Joe-noh <goflb.jh@gmail.com>
2015-03-12 11:29:50 +0900

    define struct for meterials and products

--- lib/suxuri/material.ex
+++ lib/suxuri/material.ex
@@ -1,2 +1,7 @@
 defmodule Suxuri.Material do
+  defstruct [
+    :id, :title, :description, :price, :violation, :published,
+    :published_at, :created_at, :updated_at, :dominant_rgb,
+    :original_width, :original_height, :user
+  ]
 end
--- lib/suxuri/product.ex
+++ lib/suxuri/product.ex
@@ -1,2 +1,7 @@
 defmodule Suxuri.Product do
+  defstruct [
+    :id, :title, :published, :published_at, :created_at, :updated_at,
+    :examplary_angle, :image_url, :sample_image_url, :url, :sample_url,
+    :item, :material, :sample_item_variant
+  ]
 end
