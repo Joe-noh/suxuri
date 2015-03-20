79765341e00ceeaa2c5b50f05acda701fb18e0fa
Joe-noh <goflb.jh@gmail.com>
2015-03-20 13:48:08 +0900

    add docs to Suxuri.Choice

--- lib/suxuri/choice.ex
+++ lib/suxuri/choice.ex
@@ -23,25 +23,68 @@ defmodule Suxuri.Choice do
     from_list(choices, [])
   end
 
+  @doc false
   defp from_list([head | rest], acc), do: from_list(rest, [new(head) | acc])
   defp from_list([], acc), do: Enum.reverse acc
 
+  @doc """
+  Fetch a choice from suzuri.jp.
+
+  ## Example
+
+      iex> Suxuri.Choice.get(19)
+      %Suxuri.Choice{banner_url: "...", description: "...", ...}
+  """
   @spec get(pos_integer) :: t
   def get(choice_id) when is_integer(choice_id) do
     HTTP.get!("/choices/#{choice_id}") |> new_choice
   end
 
+  @doc """
+  Fetch choices. You can specify `limit` and `offset`.
+
+  ## Example
+
+      iex> Suxuri.Choice.list
+      [%Suxuri.Choice{}, %Suxuri.Choice{}, ...]  # limit is 20. offset is 0
+
+      iex> Suxuri.Choice.list limit: 10, offset: 100
+      [%Suxuri.Choice{}, %Suxuri.Choice{}, ...]  # The list size is 10
+  """
   @spec list(Keyword.t) :: [t]
   def list(params \\ []) do
     HTTP.get!("/choices", params) |> Map.get("choices") |> from_list
   end
 
+  @doc """
+  Issue a request to create new choice.
+
+  ## Example
+
+      iex> Suxuri.Choice.create "にゃー！", description: "わがはいの思い出"
+      %Suxuri.Choice{description: "わがはいの思い出", title: "にゃー！", ...}
+
+      iex> Suxuri.Choice.create "一気にたくさん", choice_products: [
+        [product_id: 12, item_variant_id: 1],
+        [product_id: 10, item_variant_id: 81]
+      ]
+      %Suxuri.Choice{title: "一気にたくさん", products_count: 2, ...}
+  """
   @spec create(String.t, Keyword.t) :: t
   def create(title, params \\ []) do
     HTTP.post!("/choices", Keyword.merge([title: title], params))
     |> new_choice
   end
 
+  @doc """
+  Delete a choice.
+
+  ## Example
+
+      iex> Suxuri.Choice.delete 1149
+
+      iex> Suxuri.Choice.get(1149) |> Suxuri.Choice.delete
+  """
   @spec delete(pos_integer | t) :: %{}
   def delete(choice_id) when is_integer(choice_id) do
     do_delete(choice_id)
@@ -55,6 +98,14 @@ defmodule Suxuri.Choice do
     HTTP.delete!("/choices/#{choice_id}")
   end
 
+  @doc """
+  Add an item to a choice.
+
+  ## Example
+
+      iex> Suxuri.Choice.add 1149, [product_id: 12, item_variant_id: 2]
+      %Suxuri.Choice{...}
+  """
   @spec add(pos_integer | t, Keyword.t) :: t
   def add(choice, params \\ [])
   def add(choice_id, params) when is_integer(choice_id) do
@@ -68,6 +119,14 @@ defmodule Suxuri.Choice do
     HTTP.post!("/choices/#{choice_id}", params) |> new_choice
   end
 
+  @doc """
+  Remove an item from a choice.
+
+  ## Example
+
+      iex> Suxuri.Choice.remove 1149, [product_id: 12, item_variant_id: 2]
+      %Suxuri.Choice{...}
+  """
   @spec remove(pos_integer | t, Keyword.t) :: t
   def remove(choice, params \\ [])
   def remove(choice_id, params) when is_integer(choice_id) do
@@ -81,6 +140,14 @@ defmodule Suxuri.Choice do
     HTTP.post!("/choices/#{choice_id}/remove", params) |> new_choice
   end
 
+  @doc """
+  Update a choice
+
+  ## Example
+
+      iex> Suxuri.Choice.update 1149, [title: "えいやー！"]
+      %Suxuri.Choice{title: "えいやー！", ...}
+  """
   @spec update(pos_integer | t, Keyword.t) :: t
   def update(choice, params \\ [])
   def update(choice_id, params) when is_integer(choice_id) do
